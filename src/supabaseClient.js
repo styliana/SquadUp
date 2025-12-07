33a83f173b338bbe6b1a6dba7b9ddb9849234902
Aleksandra Gołek <olagolek@poczta.fm>
2025-12-07 22:33:39 +0100

    security move: keys not exposed to the world

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Twoje dane z Supabase
-const supabaseUrl = 'https://jxealqtsvczkmwplfhlh.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZWFscXRzdmN6a213cGxmaGxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwNTA2MDksImV4cCI6MjA4MDYyNjYwOX0.ye2KvLnNTvIahSERh4tdehKqNvMofDCQ5LSBM3akyAw'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
