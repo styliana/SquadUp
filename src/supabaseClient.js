4ea754eb5ce2ad3e66d6a32a880cb438ac710f4c
Aleksandra Gołek <olagolek@poczta.fm>
2025-12-08 14:49:02 +0100

    security move

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
+// Używamy zmiennych środowiskowych zamiast wpisywać klucze na sztywno
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
